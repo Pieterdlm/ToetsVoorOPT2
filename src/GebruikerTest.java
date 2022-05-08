73129ea8c4745add23757b1b64a18092efb493f5
Pieter <pieter_delamar@hotmail.com>
2022-05-08 12:45:19 +0200

    Gebruiker & Gebruikertest aangepast voor mcdc test

--- src/GebruikerTest.java
+++ src/GebruikerTest.java
@@ -18,14 +18,16 @@ class GebruikerTest {
     }
     @Test
     void magProductGroepAanmaken() {
-        Gebruiker gebruiker1 = new Gebruiker("Test",1234, true, 2020, 1);
-        Gebruiker gebruiker2 = new Gebruiker("Test",1234, false, 2014, 3);
-        Gebruiker gebruiker3 = new Gebruiker("Test",1234, false, 2012, 1);
-        Gebruiker gebruiker4 = new Gebruiker("Test",1234, false, 2020, 1);
+        Gebruiker gebruiker1 = new Gebruiker("Test1",0001, true, 2020, 1); // 1 0 0 D
+        Gebruiker gebruiker2 = new Gebruiker("Test2",0002, false, 2021, 1);// 0 0 0 D
+        Gebruiker gebruiker3 = new Gebruiker("Test3",0003, false, 2012, 2);// 0 1 1 D
+        Gebruiker gebruiker4 = new Gebruiker("Test4",0004, false, 2019, 2);// 0 0 1 D
+        Gebruiker gebruiker5 = new Gebruiker("Test5",0005, false, 2015, 1);// 0 1 0 D
 
         assertTrue(gebruiker1.MagProductGroepAanmaken(gebruiker1.getJaarInDienstTreden(),gebruiker1.getFunctieGroep(), gebruiker1.getisBeheerder()));
-        assertTrue(gebruiker2.MagProductGroepAanmaken(gebruiker2.getJaarInDienstTreden(),gebruiker2.getFunctieGroep(), gebruiker2.getisBeheerder()));
-        assertFalse(gebruiker3.MagProductGroepAanmaken(gebruiker3.getJaarInDienstTreden(),gebruiker3.getFunctieGroep(), gebruiker3.getisBeheerder()));
+        assertFalse(gebruiker2.MagProductGroepAanmaken(gebruiker2.getJaarInDienstTreden(),gebruiker2.getFunctieGroep(), gebruiker2.getisBeheerder()));
+        assertTrue(gebruiker3.MagProductGroepAanmaken(gebruiker3.getJaarInDienstTreden(),gebruiker3.getFunctieGroep(), gebruiker3.getisBeheerder()));
         assertFalse(gebruiker4.MagProductGroepAanmaken(gebruiker4.getJaarInDienstTreden(),gebruiker4.getFunctieGroep(), gebruiker4.getisBeheerder()));
+        assertFalse(gebruiker5.MagProductGroepAanmaken(gebruiker5.getJaarInDienstTreden(),gebruiker5.getFunctieGroep(), gebruiker5.getisBeheerder()));
     }
 }

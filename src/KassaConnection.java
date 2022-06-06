3b14a01995a2802c578f5bfc93619bc95eed8a98
Pieter <pieter_delamar@hotmail.com>
2022-06-06 14:50:08 +0200

    Kleine aanpassingen

---
+++ src/KassaConnection.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface KassaConnection {
+
+    ArrayList<Product> alleProducten();
+    ArrayList<WarmeDrank> alleWarmeDranken();
+    ArrayList<Tafel> alleTafels();
+}

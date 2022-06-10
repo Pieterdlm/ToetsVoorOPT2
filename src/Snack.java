402c9b23cd1b8c84e18ceb0c030ef248981fc50c
Pieter <pieter_delamar@hotmail.com>
2022-06-10 16:13:37 +0200

    Kleine aanpassingen

--- src/Snack.java
+++ src/Snack.java
@@ -1,18 +1,16 @@
 public class Snack extends Product {
     private String toevoeging;
-    private int productID;
     private static int initialSnackNum = 5000; //5000 nummers zijn allemaal snacks
 
     //constructor van childclass Snack met als parentclass Product
     public Snack(String productNaam, double productPrijs, String toevoeging) {
         super(productNaam, productPrijs);
         this.toevoeging = toevoeging;
-        this.productID = maakProductID();
     }
+
     @Override
-    public int maakProductID(){
-        productID = initialSnackNum++;
-        return productID;
+    public int maakProductID() {
+        return initialSnackNum++;
     }
 
     public void setToevoeging(String toevoeging) {
@@ -20,5 +18,3 @@ public class Snack extends Product {
     }
 
 }
-
-
--- src/Wijn.java
+++ src/Wijn.java
@@ -1,19 +1,16 @@
 public class Wijn extends Product {
     private String soortWijn;
-    private int productID;
     private static int initialWijnNum = 6000; //6000 nummers zijn allemaal wijnen
 
     //constructor van childclass Wijn met als parentclass Product
     public Wijn(String productNaam, double productPrijs, String soortWijn) {
         super(productNaam, productPrijs);
         this.soortWijn = soortWijn;
-        this.productID = maakProductID();
     }
 
     @Override
     public int maakProductID(){
-        productID = initialWijnNum++;
-        return productID;
+        return initialWijnNum++;
     }
 
     public String getSoortWijn() {

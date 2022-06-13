dcfd1501da51938dd2652daa847f0480cea39837
Pieter <pieter_delamar@hotmail.com>
2022-06-13 22:31:15 +0200

    Smells done.

--- src/KeuzeZoekJuisteTafel.java
+++ src/KeuzeZoekJuisteTafel.java
@@ -2,13 +2,12 @@ import java.util.ArrayList;
 
 public class KeuzeZoekJuisteTafel extends Keuze{
 
-    WerkInfo werkInfo = new WerkInfo(false, 2016, 2, false);
-    Gebruiker gebruiker = new Gebruiker("Sjaak de Beheerder", 2222, werkInfo);
-    KassaConnection kassaConnection = new GraanVoorVisch();
-    ArrayList<Tafel> alletafels = kassaConnection.alleTafels();
-
     @Override
     void maakKeuze() {
+        WerkInfo werkInfo = new WerkInfo(false, 2016, 2, false);
+        Gebruiker gebruiker = new Gebruiker("Sjaak de Beheerder", 2222, werkInfo);
+        KassaConnection kassaConnection = new GraanVoorVisch();
+        ArrayList<Tafel> alletafels = kassaConnection.alleTafels();
         for (Tafel tafel : alletafels){
             tafel.bestellingPlaatsen();
         }
